11820d46f17f68e58a371f0711c24deddc1cca1b
정승호 <seungho6858@gmail.com>
2018-09-07 01:58:47 +0900

    [정승호] 프로젝트 업로드

---
+++ DApp/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int a = 5;
+
+        Debug.Log("Hello ~");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
